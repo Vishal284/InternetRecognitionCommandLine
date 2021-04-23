60c28397bbcad3a9c6abec1ce44289a9d67973b9
Vishal284 <vishal284@gmail.com>
2021-04-23 15:00:53 +0200

    Added the logic to check day
    Weather can be requested for day such as today, tomorrow or even a week. Though, it does not change the Intent output, still it will be useful for actual weather request.

---
+++ IntentRecognition.h
@@ -0,0 +1,46 @@
+﻿// IntentRecognition.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+#include <string>
+
+class IntentRecognition
+{
+private:
+	
+	std::string intentInputStr;
+	std::string intentOutput;
+
+public:
+
+	// Constructor
+	IntentRecognition();
+
+	void printIntroduction();
+	void getIntentInputandProcess();
+	void identifyIntent();
+	void publishIntent();
+
+private:
+
+	bool isQuestionFormat();
+	bool isWeatherIntent();
+	bool isWeatherWithCityIntent();
+	bool isFactIntent();
+	bool isDayOrWeekSpecified();
+
+	inline std::size_t findStringInInput(const std::string& searchStr)
+	{
+		return intentInputStr.find(searchStr);
+	};
+
+public:
+
+	// Functions introduced to support unit test	
+	void setIntentInput(const std::string& intentStr);
+	void getIntentOutput(std::string& intentStr);	
+};
